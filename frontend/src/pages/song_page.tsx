258b3171bb5a25e2e62ccc37546339116d342b89
Priyansh B <priyanshmagic@gmail.com>
2025-01-18 21:22:02 +0800

    update formatted text display

--- frontend/src/pages/song_page.tsx
+++ frontend/src/pages/song_page.tsx
@@ -9,14 +9,20 @@ interface SongPageProps {
 const SongPage: React.FC<SongPageProps> = ({ text }) => {
   const navigate = useNavigate();
   const location = useLocation();
-  const texty = location.state?.extractedText || "No text provided.";
+  // const texty = location.state?.extractedText || "No text provided.";
+  const texty = location.state?.formattedText || "No text provided.";
+
+  const formattedText = texty.replace(/\n/g, "<br />");
 
   return (
     <div className="container">
       <div className="white-background">
         <h3 className="font-bold">Extracted Text from PDF</h3>
-        <span className="bg-highlight-yellow">{texty}</span> {/* Highlight specific sentence*/}
-
+        {/* <span className="bg-highlight-yellow">{texty}</span> */}
+        <div
+          className="bg-highlight-yellow"
+          dangerouslySetInnerHTML={{ __html: formattedText }}
+        />
       </div>
       <BackToHome />
     </div>
